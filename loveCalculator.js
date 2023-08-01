1c655106a33b150403dab189506cf67bd8bfb566
João Ricardo <ricardo.forttunato@gmail.com>
2023-08-01 11:07:10 -0300

    Add loveCalculator Exemple file, issue on  prompt

---
+++ loveCalculator.js
@@ -0,0 +1,6 @@
+var nameLover = prompt("Input your name. \n");
+var nameCrush = prompt("Input your crush name. \n");
+
+var loveScore = Math.random() * 100 + 1;
+
+console.log("Your love score is " + Math.floor(loveScore) + "%");
