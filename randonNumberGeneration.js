7e3d46864b9e48eb5be1508966d133546f939123
João Ricardo <ricardo.forttunato@gmail.com>
2023-08-01 10:36:18 -0300

    Exemple random function

---
+++ randonNumberGeneration.js
@@ -0,0 +1,4 @@
+var n = Math.random();  // Gera um número aleatório usando a função random do módulo Math.
+n = n * 12 + 1; // Gera um número aleatório entre 1 e 12
+n = Math.floor(n); // Arredonda para baixo o número gerado.
+console.log(n);
